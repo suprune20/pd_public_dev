21438cedf3b45d4582255a26e44cab97f9bad076
corrupt <vetal.lg@tut.by>
2013-12-31 02:30:31 +0300

    Apply/clear catalog filters

--- app/scripts/controllers/catalog.js
+++ app/scripts/controllers/catalog.js
@@ -2,7 +2,13 @@
 
 angular.module('pdApp')
   .controller('CatalogCtrl', function ($scope, $modal, $routeParams, $location, Catalog) {
+    $scope.filters = {};
     $scope.catalog = new Catalog();
+    // Get filters data
+    $scope.categoriesFilter = $scope.catalog.getCategories();
+    $scope.suppliersFilter = $scope.catalog.getFilters().suppliers;
+    $scope.placesFilter = $scope.catalog.getFilters().places;
+
     function openProductDetailsModal(productId) {
       $location.search('productId', productId);
       var productData = $scope.catalog.getProduct(productId);
@@ -26,11 +32,28 @@ angular.module('pdApp')
         });
     }
 
-    // Restore previous filters/queryParams states
+    // Restore product modal details from query params
     if (_.has($routeParams, 'productId')) {
       openProductDetailsModal($routeParams.productId);
     }
+    // Restore filters from query params after reload
+    _.forEach($routeParams, function (value, key) {
+      if (/^filter_/.test(key)) {
+        $scope.filters[key.replace(/^filter_/, '')] = !isNaN(value) ? parseInt(value, 10) : value;
+      }
+    });
 
     $scope.openProductDetailsModal = openProductDetailsModal;
+    $scope.applyFilters = function () {
+      _.forEach($scope.filters, function (value, filterName) {
+        $location.search('filter_' + filterName, value);
+      });
+    };
+    $scope.clearFilters = function () {
+      _.forEach($scope.filters, function (value, filterName) {
+        $location.search('filter_' + filterName, null);
+      });
+      $scope.filters = {};
+    };
   })
 ;
