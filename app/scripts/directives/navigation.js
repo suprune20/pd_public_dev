27f2ff2cff9efe6083feb0faa8cab744cd0c19ef
corrupt <vetal.lg@tut.by>
2014-01-11 00:08:55 +0300

    Add directive for setting current active menu item

---
+++ app/scripts/directives/navigation.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('pdApp')
+  .directive('activeLink', function ($location) {
+    return {
+      restrict: 'AC',
+      link: function (scope, iElement, iAttrs) {
+        scope.$watch(function () {
+          return $location.path();
+        }, function (path) {
+          var url = iElement.find('a').attr('href'),
+            activeClass = iAttrs.activeLink || 'active';
+
+          if (url) {
+            url = url.substring(1);
+          }
+
+          if (path == url) {
+            iElement.addClass(activeClass);
+          } else {
+            iElement.removeClass(activeClass);
+          }
+        });
+      }
+    };
+  })
+;
