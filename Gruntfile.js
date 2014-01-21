859cb0b3c19103f1d465fa35523192d9fcc8dda9
corrupt <vetal.lg@tut.by>
2014-01-22 01:23:38 +0300

    Add unit tests for security service

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
       },
       jsTest: {
         files: ['test/spec/**/*.js'],
-        tasks: ['newer:jshint:test', 'karma']
+        tasks: ['newer:jshint:test', 'karma:unit']
       },
       compass: {
         files: ['<%= yeoman.app %>/styles/**/*.{scss,sass}'],
