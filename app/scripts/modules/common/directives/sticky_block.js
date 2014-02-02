2003d7f9d9d13da09314db246d2ef22b9d7a375b
corrupt <vetal.lg@tut.by>
2014-02-02 22:51:49 +0300

    Add zero-timeout for sticky block directive

--- app/scripts/modules/common/directives/sticky_block.js
+++ app/scripts/modules/common/directives/sticky_block.js
@@ -1,25 +1,29 @@
 'use strict';
 
 angular.module('pdCommon')
-  .directive('pbStickyBlock', function ($window, $log) {
+  .directive('pbStickyBlock', function ($window, $log, $timeout) {
     return {
       restrict: 'AE',
       link: function (scope, iElement, iAttrs) {
-        var stickyTopPosition = iAttrs.hasOwnProperty('topPosition') ? parseInt(iAttrs.topPosition, 10) : 0,
+        // Wrap into zero-timeout-fix: wrong block position coords after change route, maybe angularjs animation
+        // ToDo: Check on a new angular.js animation version > 1.2.10
+        $timeout(function () {
+          var stickyTopPosition = iAttrs.hasOwnProperty('topPosition') ? parseInt(iAttrs.topPosition, 10) : 0,
             stickyScrollDelta = iElement.offset().top - stickyTopPosition;
 
-        if (stickyScrollDelta <= 0) {
-          $log.warn('pbStickyBlock directive: needed stick position should be higher than current');
-          return;
-        }
-
-        angular.element($window).on('scroll', function () {
-          if (this.pageYOffset >= stickyScrollDelta) {
-            iElement.addClass('pb-sticky-block');
-          } else {
-            iElement.removeClass('pb-sticky-block');
+          if (stickyScrollDelta <= 0) {
+            $log.warn('pbStickyBlock directive: needed stick position should be higher than current');
+            return;
           }
-        });
+
+          angular.element($window).on('scroll', function () {
+            if (this.pageYOffset >= stickyScrollDelta) {
+              iElement.addClass('pb-sticky-block');
+            } else {
+              iElement.removeClass('pb-sticky-block');
+            }
+          });
+        }, 0);
       }
     };
   })
