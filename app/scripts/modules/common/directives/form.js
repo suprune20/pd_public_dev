2e79518eedc1dea890ab1054b85f3f148672ef6b
corrupt <vetal.lg@tut.by>
2014-02-04 23:28:16 +0300

    Autofill forms in chrome

--- app/scripts/modules/common/directives/form.js
+++ app/scripts/modules/common/directives/form.js
@@ -1,13 +1,22 @@
 'use strict';
 
 angular.module('pdCommon')
-  .directive('fixAutofill', function () {
+  .directive('fixAutofill', function ($interval) {
     return function (scope, iElement, iAttrs) {
+      var intervalPromise;
+
       iAttrs.$observe('fixAutofill', function (needFix) {
-        needFix = scope.$eval(needFix);
-        if (true === needFix) {
-          iElement.find('input, textarea, select').trigger('change');
+        if (true === scope.$eval(needFix)) {
+          intervalPromise = $interval(function () {
+            iElement.find('input').trigger('change');
+          }, 500);
+        } else if (intervalPromise) {
+          // Clear intervals between needFix callbacks callbacks
+          $interval.cancel(intervalPromise);
         }
+      });
+      scope.$on('$destroy', function () {
+        $interval.cancel(intervalPromise);
       });
     };
   })
