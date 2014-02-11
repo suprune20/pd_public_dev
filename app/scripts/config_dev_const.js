4e9a77a4407f96c47681836ff30bbfd700b456ec
corrupt <vetal.lg@tut.by>
2014-02-11 23:34:27 +0300

    Add ravenDevelopment constant for dev config

--- app/scripts/config_dev_const.js
+++ app/scripts/config_dev_const.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('pdConfig')
+  .constant('ravenDevelopment', false)
   .constant('serverConfig', {
     serverHost: 'http://org.dev.pohoronnoedelo.ru/',
     cookieDomain: '.pohoronnoedelo.ru'
