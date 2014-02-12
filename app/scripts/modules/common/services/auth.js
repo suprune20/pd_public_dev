839ca5f62dfeed4da1ddb6b9630dc46f31256b8e
corrupt <vetal.lg@tut.by>
2014-02-13 00:04:40 +0300

    Do not send signout api request

--- app/scripts/modules/common/services/auth.js
+++ app/scripts/modules/common/services/auth.js
@@ -36,13 +36,10 @@ angular.module('pdCommon')
           });
       },
       signout = function () {
-        return $http.post(pdConfig.apiEndpoint + 'auth/signout', {}, {tracker: 'commonLoadingTracker'})
-          .then(function () {
-            ipCookie('pdsession', 'wrong_session', {domain: pdConfig.AUTH_COOKIE_DOMAIN});
-            storage.remove(pdConfig.AUTH_TOKEN_KEY);
-            storage.remove(pdConfig.AUTH_ROLES_KEY);
-            $rootScope.$broadcast('auth.signout');
-          });
+        ipCookie('pdsession', 'wrong_session', {domain: pdConfig.AUTH_COOKIE_DOMAIN});
+        storage.remove(pdConfig.AUTH_TOKEN_KEY);
+        storage.remove(pdConfig.AUTH_ROLES_KEY);
+        $rootScope.$broadcast('auth.signout');
       },
       isAuthenticated = function () {
         return !!storage.get(pdConfig.AUTH_TOKEN_KEY);
