e9934a45c8e7f03942efacd9ad7180bbf107afc9
Corrupt <vitaly.zhloba@gmail.com>
2014-03-06 01:17:24 +0300

    Add ability to change notifications email

--- app/scripts/modules/frontend/services/user.js
+++ app/scripts/modules/frontend/services/user.js
@@ -67,9 +67,11 @@ angular.module('pdFrontend')
           .then(function () {
             // Update user profile in locale storage
             var currentUserData = storage.get(pdConfig.AUTH_PROFILE_KEY);
-            currentUserData.profile.mainPhone = _.has(settingsData, 'mainPhone') ?
-              settingsData.mainPhone :
-              currentUserData.profile.mainPhone;
+
+            currentUserData.profile.mainPhone = settingsData.mainPhone || currentUserData.profile.mainPhone;
+            currentUserData.profile.email = _.has(settingsData, 'email') ?
+              settingsData.email :
+              currentUserData.profile.email;
             storage.set(pdConfig.AUTH_PROFILE_KEY, currentUserData);
           }, function (errorResp) {
             var respData = errorResp.data;
