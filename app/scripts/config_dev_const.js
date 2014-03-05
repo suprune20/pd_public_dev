2957bbd04919a0033a0156955ecf95aa80a30c60
Corrupt <vitaly.zhloba@gmail.com>
2014-03-05 23:53:34 +0300

    Set https for org.dev api

--- app/scripts/config_dev_const.js
+++ app/scripts/config_dev_const.js
@@ -3,7 +3,7 @@
 angular.module('pdConfig')
   .constant('ravenDevelopment', false)
   .constant('serverConfig', {
-    serverHost: 'http://org.dev.pohoronnoedelo.ru/',
+    serverHost: 'https://org.dev.pohoronnoedelo.ru/',
     cookieDomain: '.pohoronnoedelo.ru'
   })
 ;
