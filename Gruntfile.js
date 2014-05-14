ea41e57d872aa892fd479c82674ad9b528b9ded8
Corrupt <vitaly.zhloba@gmail.com>
2014-05-14 10:15:17 +0300

    Exclude yandex marker custom marker from rev task

--- Gruntfile.js
+++ Gruntfile.js
@@ -210,7 +210,9 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
+            '<%= yeoman.dist %>/styles/fonts/*',
+            // Exclude any images
+            '!<%= yeoman.dist %>/images/blueCircleDotIcon.png' // Yandex map custom marker
           ]
         }
       }
