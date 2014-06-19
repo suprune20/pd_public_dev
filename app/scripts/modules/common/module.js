2639a5064aa66934d1816ab5493512f561f03506
Corrupt <vitaly.zhloba@gmail.com>
2014-06-20 00:09:39 +0300

    Move directive decorators into external file

--- app/scripts/modules/common/module.js
+++ app/scripts/modules/common/module.js
@@ -11,37 +11,8 @@ angular.module('pdCommon', [
     'angularFileUpload',
     'checklist-model'
   ])
-  .config(function (growlProvider, $provide) {
+  .config(function (growlProvider) {
     growlProvider.globalTimeToLive(5000);
-    // Allow dynamic set names for form elements
-    $provide.decorator('ngModelDirective', function ($delegate) {
-      var ngModel = $delegate[0], controller = ngModel.controller;
-      ngModel.controller = ['$scope', '$element', '$attrs', '$injector', function (scope, element, attrs, $injector) {
-        var $interpolate = $injector.get('$interpolate');
-        attrs.$set('name', $interpolate(attrs.name || '')(scope));
-        $injector.invoke(controller, this, {
-          '$scope': scope,
-          '$element': element,
-          '$attrs': attrs
-        });
-      }];
-
-      return $delegate;
-    });
-    $provide.decorator('formDirective', function ($delegate) {
-      var form = $delegate[0], controller = form.controller;
-      form.controller = ['$scope', '$element', '$attrs', '$injector', function (scope, element, attrs, $injector) {
-        var $interpolate = $injector.get('$interpolate');
-        attrs.$set('name', $interpolate(attrs.name || attrs.ngForm || '')(scope));
-        $injector.invoke(controller, this, {
-          '$scope': scope,
-          '$element': element,
-          '$attrs': attrs
-        });
-      }];
-
-      return $delegate;
-    });
   })
   .run(function ($rootScope, promiseTracker) {
     $rootScope.commonLoadingTracker = promiseTracker('commonLoadingTracker');
