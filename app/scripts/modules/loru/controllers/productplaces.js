8b55cc441c5428b8c6f642a9b34b3b8dcaea6e21
Corrupt <vitaly.zhloba@gmail.com>
2014-07-08 00:55:30 +0300

    Fix typo

--- app/scripts/modules/loru/controllers/productplaces.js
+++ app/scripts/modules/loru/controllers/productplaces.js
@@ -5,7 +5,7 @@ angular.module('pdLoru')
   .controller('LoruAdvertisementCtrl', function ($scope, $q, advertisement, objectsDiff, modalNotifications, $location) {
     // Check is exist my stores for show alert message
     advertisement.isExistsMyStores().catch(function () {
-      modalNotifications.confirm('У Вас не добавлено ниодного склада. Хотите добавить сейчас?', 'Отсутствуют склады')
+      modalNotifications.confirm('У Вас не добавлено ни одного склада. Хотите добавить сейчас?', 'Отсутствуют склады')
         .then(function () {
           $location.path('/loru/orgplaces');
         });
