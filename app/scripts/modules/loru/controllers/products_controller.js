38445e573e28b0be24dd032ff113f752cebb2a6a
Corrupt <vitaly.zhloba@gmail.com>
2014-09-25 23:12:31 +0300

    Allow edit prices on products list page

--- app/scripts/modules/loru/controllers/products_controller.js
+++ app/scripts/modules/loru/controllers/products_controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pdLoru')
-  .controller('LoruProductsListCtrl', function ($scope, categories, loruProducts) {
+  .controller('LoruProductsListCtrl', function ($scope, categories, loruProducts, $q, growl) {
     $scope.categories = categories;
     $scope.applyFilter = function () {
       var filters = _.cloneDeep($scope.filters);
@@ -10,15 +10,28 @@ angular.module('pdLoru')
       loruProducts.getProducts(filters).then(function (products) { $scope.products = products; });
     };
     $scope.applyFilter();
-    $scope.updateProductVisibility = function (product) {
+    $scope.updateProduct = function (product) {
+      // Reset showing flag if has been resetting prices
+      if (!product.retailPrice) {
+        product.isShownInRetailCatalog = false;
+      }
+      if (!product.tradePrice) {
+        product.isShownInTradeCatalog = false;
+      }
+
       var updateModel = _.transform(product, function (result, value, key) {
-        if (_.contains(['id', 'isShownInRetailCatalog', 'isShownInTradeCatalog'], key)) {
+        if (_.contains(['id', 'isShownInRetailCatalog', 'isShownInTradeCatalog', 'tradePrice', 'retailPrice'], key)) {
           result[key] = value;
           return result;
         }
       }, {});
       $scope.disabledVisibilityControls = true;
-      loruProducts.saveProduct(updateModel)
+
+      return loruProducts.saveProduct(updateModel)
+        .catch(function (errorData) {
+          growl.addErrorMessage(errorData.message || 'Произошла неизвестная ошибка');
+          return $q.reject();
+        })
         .finally(function () { $scope.disabledVisibilityControls = false; });
     };
   })
