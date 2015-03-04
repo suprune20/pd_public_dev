d6cbc5a645bcd778ea7e1e4738018e13eff3be51
Corrupt <vitaly.zhloba@gmail.com>
2015-03-05 00:07:03 +0300

    Use shop api from server for shop details page

--- app/scripts/modules/hram/shop/controllers/shop_details_controller.js
+++ app/scripts/modules/hram/shop/controllers/shop_details_controller.js
@@ -9,56 +9,11 @@ angular.module('pdHram')
         resolve: {
           shopData: function ($stateParams, shopProvider) {
             return shopProvider.getShopDetails($stateParams.shopId);
-
-            return {
-              id: 123,
-              title: 'Магазин 123',
-              gallery: [
-                'http://lorempixel.com/400/200',
-                'http://lorempixel.com/400/210',
-                'http://lorempixel.com/400/205',
-                'http://lorempixel.com/400/206'
-              ],
-              services: [
-                {
-                  id: 1,
-                  title: 'Заказ фотографии',
-                  price: parseFloat(Math.random() * 1000).toFixed(2)
-                },
-                {
-                  id: 2,
-                  title: 'Венок',
-                  price: parseFloat(Math.random() * 1000).toFixed(2)
-                }
-              ],
-              reviews: [
-                {
-                  user: {
-                    firstName: 'Иван',
-                    lastName: 'Иванов'
-                  },
-                  title: 'Ужасно',
-                  createdAt: '2015-01-24T23:57:02+00:00',
-                  comment: {
-                    positive: 'Очень быстро выполнился заказ',
-                    negative: 'Ужасное качество исполнения'
-                  }
-                }
-              ]
-            };
           }
         },
         controller: ['$scope', 'shopData', '$modal', function ($scope, shopData, $modal) {
           $scope.shop = shopData;
-          $scope.services = _.map(shopData.services, function (service) {
-            return {
-              service: service,
-              isSelected: false
-            };
-          });
-          $scope.$watch('services', function (services) {
-            $scope.isSelectedAnyServices = _.some(services, 'isSelected');
-          }, true);
+          $scope.selectedServices = [];
 
           $scope.placeOrder = function () {
             $modal.open({
--- app/scripts/modules/hram/shop/services/shop_service.js
+++ app/scripts/modules/hram/shop/services/shop_service.js
@@ -18,11 +18,23 @@ angular.module('pdHram')
           .then(function (response) {
             return response.data;
           });
+      },
+      getShopReviews: function (shopId) {
+        return $http.get(pdConfig.apiEndpoint + 'shops/' + shopId + '/reviews')
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      getShopGallery: function (shopId) {
+        return $http.get(pdConfig.apiEndpoint + 'shops/' + shopId + '/gallery')
+          .then(function (response) {
+            return response.data;
+          });
       }
     };
   })
 
-  .service('shopProvider', function (shopApi, pdFrontendCatalogApi) {
+  .service('shopProvider', function (shopApi, pdFrontendCatalogApi, $q) {
     return {
       getShopsCollection: function (queryString, categories) {
         return shopApi.getShops(queryString, categories);
@@ -31,7 +43,32 @@ angular.module('pdHram')
         return pdFrontendCatalogApi.getCategories();
       },
       getShopDetails: function (shopId) {
-        return shopApi.getShop(shopId);
+        return $q.all([
+          shopApi.getShop(shopId),
+          this.getShopProducts(shopId),
+          this.getShopReviews(shopId),
+          this.getShopGallery(shopId)
+        ]).then(function (responses) {
+          var detailsModel = responses[0];
+
+          detailsModel.services = responses[1];
+          detailsModel.reviews = responses[2];
+          detailsModel.gallery = responses[3];
+
+          return detailsModel;
+        });
+      },
+      getShopProducts: function (shopId) {
+        return pdFrontendCatalogApi.getProducts({
+          'filter[supplier][]': shopId,
+          'filter[isAvailableForVisitOrder]': true
+        });
+      },
+      getShopReviews: function (shopId) {
+        return shopApi.getShopReviews(shopId);
+      },
+      getShopGallery: function (shopId) {
+        return shopApi.getShopGallery(shopId);
       }
     };
   })
